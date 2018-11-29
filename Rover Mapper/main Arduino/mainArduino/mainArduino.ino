790f594b0f8bfa8aa6696ff3ebcd0ed538ffd79b
FerraChris01 <ferrareis.christian@gmail.com>
2018-11-29 23:26:24 +0100

    Add files via upload

--- Rover Mapper/main Arduino/mainArduino/mainArduino.ino
+++ Rover Mapper/main Arduino/mainArduino/mainArduino.ino
@@ -1,14 +1,25 @@
+#include <Bluetooth.h>
+
 #include <bussola.h>
 
 #include <ultrasuoni.h>
+
+Bluetooth bt;
 
 Ultrasuoni ultra;
 
 Bussola bussola;
 
+int direzioneGradi;
+
 void setup() { 
   Serial.begin(9600);  
+  bt = Bluetooth();
+  bussola = Bussola();
+  ultra = Ultrasuoni();
+  direzioneGradi = bussola.getDegree();
   bussola.avanti();
+  ultra.calcolaDistanze();
 }
 
 void loop() { 
@@ -17,27 +28,35 @@ void loop() {
      bussola.checkRotation();
      ultra.calcolaDistanze();     
      bussola.checkRotation();
-     ultra.sendDxSx();
+     bt.sendData(ultra.getDx(), ultra.getSx(), bussola.getDegree());
      bussola.checkRotation();
-  }
+     direzioneGradi = bussola.getDegree();
+  }  
   else
   {
-     ultra.calcolaDistanze();  
-     ultra.sendDxSx();
-     //delay maggiore
+    ultra.calcolaDistanze(); 
+     //bt.sendData(ultra.getDx(), ultra.getSx(), bussola.getDegree());
+     //bussola.straighten();
      trovaPercorsoMigliore(); 
-  }       
+  }    
+  Serial.println("f" + String(ultra.getFront()));
+  Serial.println("d" + String(ultra.getDx()));
+  Serial.println("s" + String(ultra.getSx()));
+  delay(500);
 }
 
-void trovaPercorsoMigliore() {
+void trovaPercorsoMigliore() 
+{
   //si deve sempre fare una media tra distanza a destra e sinistra NECESSARIO SENSORE POSTERIORE
   if (ultra.getFront() < 10 && ultra.getFront() != - 1) bussola.stopMotors();
-  //if (ultra.getDx() == -1)
-  //   bussola.routineRuotaDx(90);
- // else if (ultra.getFront() < 5 && ultra.getDx() != -1)
- //   bussola.routineRuotaSx(180);
-  //else if (ultra.getFront() < 5 && ultra.getSx() != -1)
-  //   bussola.routineRuotaDx(180); 
-  //else if (ultra.getFront() < 5 && ultra.getDx() != -1)
-   //  bussola.routineRuotaSx(90);
+ 
+  if (ultra.getDx() == -1)
+     bussola.routineRuotaDx(90);
+  else if (ultra.getFront() < 10.0 && ultra.getDx() != -1)
+    bussola.routineRuotaSx(180);
+  else if (ultra.getFront() < 10.0 && ultra.getSx() != -1)
+     bussola.routineRuotaDx(180); 
+  else if (ultra.getFront() < 10.0 && ultra.getDx() != -1)
+     bussola.routineRuotaSx(90);
+    
 }
