811cf1f0ca62cafec6301c1dff2472ddd66e05ef
FerraChris01 <ferrareis.christian@gmail.com>
2018-12-01 16:15:48 +0100

    Update mainArduino.ino

--- Rover Mapper/main Arduino/mainArduino/mainArduino.ino
+++ Rover Mapper/main Arduino/mainArduino/mainArduino.ino
@@ -23,7 +23,7 @@ void setup() {
 }
 
 void loop() { 
-  if (bussola.stoRuotando())                //il numero di distanze calcolate varia a seconda della velocità (del moto o angolare)
+  /*if (bussola.stoRuotando())                //il numero di distanze calcolate varia a seconda della velocità (del moto o angolare)
   { 
      bussola.checkRotation();
      ultra.calcolaDistanze();     
@@ -39,24 +39,21 @@ void loop() {
      //bussola.straighten();
      trovaPercorsoMigliore(); 
   }    
-  Serial.println("f" + String(ultra.getFront()));
-  Serial.println("d" + String(ultra.getDx()));
-  Serial.println("s" + String(ultra.getSx()));
-  delay(500);
+  */
+  trovaPercorsoMigliore(); 
 }
 
 void trovaPercorsoMigliore() 
 {
-  //si deve sempre fare una media tra distanza a destra e sinistra NECESSARIO SENSORE POSTERIORE
-  if (ultra.getFront() < 10 && ultra.getFront() != - 1) bussola.stopMotors();
+  if (ultra.getFront() < 13 && ultra.getFront() != - 1) bussola.stopMotors();
  
   if (ultra.getDx() == -1)
      bussola.routineRuotaDx(90);
-  else if (ultra.getFront() < 10.0 && ultra.getDx() != -1)
+  else if (ultra.getFront() < 13 && ultra.getDx() != -1)
     bussola.routineRuotaSx(180);
-  else if (ultra.getFront() < 10.0 && ultra.getSx() != -1)
+  else if (ultra.getFront() < 13 && ultra.getSx() != -1)
      bussola.routineRuotaDx(180); 
-  else if (ultra.getFront() < 10.0 && ultra.getDx() != -1)
+  else if (ultra.getFront() < 13 && ultra.getDx() != -1)
      bussola.routineRuotaSx(90);
     
 }
